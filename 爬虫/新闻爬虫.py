fcc5d8ce74d88e58cc1af097e40474dda63edf7b
demacialux <a15260414413@gmail.com>
2018-11-29 17:01:24 +0800

    新闻爬虫获取标题和链接，并且成功去除重复链接和额外信息

---
+++ 爬虫/新闻爬虫.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://news.sina.com.cn/world/'
+r = requests.get(url)
+html = r.text.encode("latin1").decode("utf-8")
+soup = BeautifulSoup(html,'html.parser')
+
+for news in  soup.find_all('a',target='_blank'):
+    if news in soup.find_all('a',{'class':'img'}):
+        pass
+    elif '详细' == news.get_text():
+        pass
+    elif '更多' == news.get_text():
+        pass
+    else:
+        title = news.get_text()
+        line = news.get('href')
+        title_lines = title.strip() +' ' + line
+        print(title_lines)
